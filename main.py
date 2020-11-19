124ddc17ceb75a9c1aa4e0c974a680f4d8c81d72
mjcortejo <mark_jethro27@yahoo.com>
2020-11-19 20:47:06 +0800

    fix: fix approach for traversing the doubly linked list

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from structure import Node, DoublyLinkedList
 
-automaton = DoublyLinkedList()
+input_string = DoublyLinkedList()
+input_string.append("#")
+input_string.append("a")
+input_string.append("b")
+input_string.append("c")
+input_string.append("#")
+current = input_string.head
+
+if current.data == "#":
+    current = current.next
+
+while (current.data != "#"):
+    print(current.data)
+    # last = current
+    current = current.next
