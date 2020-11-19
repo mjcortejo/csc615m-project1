acc547fde663ecf8ac062b0a46307edae2ae6b23
mjcortejo <mark_jethro27@yahoo.com>
2020-11-19 20:46:54 +0800

    fix: temporarily disable push since i'm not gonna need it no more

--- structure.py
+++ structure.py
@@ -9,17 +9,30 @@ class DoublyLinkedList():
     def __init__(self):
         self.head = None
 
-    def push(self, data):
+    # def push(self, data):
+    #     node = Node(data)
+    #     node.next = self.head
+    #     if self.head is not None:
+    #         self.head.prev = node
+    #     self.head = node
+    #     print(f"Current head: {self.head.data}")
+
+    def append(self, data):
         node = Node(data)
-        node.next = self.head
-        if self.head is not None:
-            self.head.prev = node
-        self.head = node
+        node.next = None
+        if self.head is None:
+            node.prev = None
+            self.head = node
+            return
+        last = self.head
+        while (last.next is not None):
+            last = last.next
+        last.next = node
+        node.prev = last
+        return
 
     def listprint(self, node):
         while (node is not None):
             print(node.data),
             last = node
             node = node.next
-
-            
