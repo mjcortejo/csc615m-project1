341393da113dc25642a1942f7be3faf1c6df2ec4
Mark Jethro Cortejo <mark.cortejo@senti.com.ph>
2020-11-19 18:38:29 +0800

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+def automaton(input_string):
+    for each_character in input_string:
+
+if __name__ == "__main__":
+    automaton("Hello world")
---
+++ structure.py
@@ -0,0 +1,22 @@
+class Node():
+    def __init__(self, data, prev=None, next=None)
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList():
+    def __init__(self, input_string):
+        self.head = None
+
+    def push(self, data):
+        node = Node(data)
+        node.next = self.head
+        if self.head is not None:
+            self.head.prev = node
+        self.head = node
+
+    def listprint(self, node):
+        while (node is not None):
+            print(node.data),
+            last = node
+            node = node.next
